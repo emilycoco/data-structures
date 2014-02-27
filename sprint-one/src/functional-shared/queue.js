6952413361d9eb6e63d67f021a493dbc156e1612
Hack Reactor Students <emilymcoco@gmail.com>
2014-02-26 16:42:59 -0800

    started fixing.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,5 +1,32 @@
 var makeQueue = function(){
-  // Hey! Copy your code from src/functional/queue.js and paste it here
+  var instance = {};
+
+  // Use an object with numeric keys to store values
+  var storage = {};
+  var firstIndex = 0;
+  var lastIndex = 0;
+
+  // Implement the methods below
+
+  instance.enqueue = function(value){
+    storage[lastIndex] = value;
+    lastIndex++;
+  };
+
+  instance.dequeue = function(){
+    if(storage[firstIndex] !== undefined){
+      var returnValue = storage[firstIndex];
+      delete storage[firstIndex];
+      firstIndex++;
+      return returnValue;
+    }
+  };
+
+  instance.size = function(){
+    return lastIndex - firstIndex;
+  };
+
+  return instance;
 };
 
 var queueMethods = {};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,5 +1,42 @@
-var makeStack = function() {
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+// Create an object that holds the methods that will be shared by all instances of the class.
+// You'll need to use the keyword this in your methods.
+// Use _.extend to copy the methods onto the instance.
+
+var stackMethods = {
+  push : function(value){
+
+    this.storage[this.size++] = value;
+  },
+
+  pop : function(){
+    if (this.size > 0) {
+      var returnValue = this.storage[this.size - 1];
+      delete this.storage[this.size - 1];
+      this.size--;
+      return returnValue;
+    }
+  },
+
+  size : function(){
+    return this.size;
+  }
 };
-  
-var stackMethods = {};
+
+
+var makeStack = function(){
+  var instance = {};
+
+  instance.storage = {};
+  instance.size = 0;
+  // Use an object with numeric keys to store values
+  // Hint: set an initial value here
+  _.extend(instance, stackMethods);
+
+  // Implement the methods below
+
+
+  return instance;
+};
+
+// var myS = makeStack();
+// myS.push(5);
