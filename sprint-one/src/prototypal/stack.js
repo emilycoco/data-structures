ef88476bc033cbb49172ee465807beb266cb16c4
Hack Reactor Students <emilymcoco@gmail.com>
2014-02-26 21:19:37 -0800

    started prototypal.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,5 +1,35 @@
-var makeStack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+
+
+
+
+var makeStack = function(){
+  var instance = Object.create(makeStack.stuff);
+
+  instance.storage = {};
+  instance.quant = 0;
+
+  return instance;
 };
 
-var stackMethods = {};
+makeStack.stuff = {};
+
+
+  makeStack.stuff.push = function(value){
+    this.storage[this.quant++] = value;
+  };
+
+  makeStack.stuff.pop = function(){
+    if (this.quant > 0) {
+      var returnValue = this.storage[this.quant - 1];
+      delete this.storage[this.quant - 1];
+      this.quant--;
+      return returnValue;
+    }
+  };
+
+  makeStack.stuff.size = function(){
+    return this.quant;
+  };
+
+
+
