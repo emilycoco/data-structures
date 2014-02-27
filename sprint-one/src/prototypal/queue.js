67c5a3cdecf54cff2fba341dec996ea968b31e29
Hack Reactor Students <emilymcoco@gmail.com>
2014-02-27 10:10:18 -0800

    Fixed both prototypal instantiations.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,5 +1,30 @@
-var makeQueue = function() {
-  // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+var makeQueue = function(){
+  var instance = Object.create(queueMethods);
+
+  // Use an object with numeric keys to store values
+  instance.storage = {};
+  instance.firstIndex = 0;
+  instance.lastIndex = 0;
+
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue : function(value){
+    this.storage[this.lastIndex] = value;
+    this.lastIndex++;
+  },
+
+  dequeue : function(){
+    if(this.storage[this.firstIndex] !== undefined){
+      var returnValue = this.storage[this.firstIndex];
+      delete this.storage[this.firstIndex];
+      this.firstIndex++;
+      return returnValue;
+    }
+  },
+
+  size : function(){
+    return this.lastIndex - this.firstIndex;
+  }
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,9 +1,5 @@
-
-
-
-
 var makeStack = function(){
-  var instance = Object.create(makeStack.stuff);
+  var instance = Object.create(stackMethods);
 
   instance.storage = {};
   instance.quant = 0;
@@ -11,25 +7,24 @@ var makeStack = function(){
   return instance;
 };
 
-makeStack.stuff = {};
-
-
-  makeStack.stuff.push = function(value){
+var stackMethods = {
+  push : function(value){
     this.storage[this.quant++] = value;
-  };
+  },
 
-  makeStack.stuff.pop = function(){
+  pop : function(){
     if (this.quant > 0) {
       var returnValue = this.storage[this.quant - 1];
       delete this.storage[this.quant - 1];
       this.quant--;
       return returnValue;
     }
-  };
+  },
 
-  makeStack.stuff.size = function(){
+  size : function(){
     return this.quant;
-  };
+  }
 
+};
 
 
