321c85a0e6e93bb289eceb7568a1b519aa0e6fce
Hack Reactor Students <emilymcoco@gmail.com>
2014-02-28 13:29:28 -0800

    Fixed tree structure.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,6 +2,8 @@ var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
   newTree.children = undefined;
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
   return newTree;
 };
 
@@ -9,8 +11,24 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  if(this.children === undefined){
+    this.children = [];
+  }
+  this.children.push(makeTree(value));
 };
 
 treeMethods.contains = function(target){
+  if(this.value === target){
+    return true;
+  } else {
+    if(this.children){
+      for (var i = 0; i < this.children.length; i++) {
+        if(this.children[i].contains(target)){
+          return true;
+        }
+      }
+    }
+  }
+  return false;
 };
 
