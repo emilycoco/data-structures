f3a39677741f8c5d3d5df81f01802f6f9ecdc4a5
Hack Reactor Students <emilymcoco@gmail.com>
2014-02-27 17:47:11 -0800

    Fixed linkedlist.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,40 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode =  makeNode(value);
+    if(list.tail === null){
+      list.head = newNode;
+    } else {
+      list.tail.next = newNode;
+    }
+    list.tail = newNode;
   };
 
+
   list.removeHead = function(){
+    if(list.head !== null){
+      var headValue = list.head.value;
+      if(list.head.next === null){
+        delete list.head;
+        list.head = null;
+        list.tail = null;
+      }
+      var newHead = list.head.next;
+      delete list.head;
+      list.head = newHead;
+      return headValue;
+    }
   };
 
   list.contains = function(target, node){
+    var start = node || list.head;
+    while (target !== start.value) {
+      if (start.next === null) {
+        return false;
+      }
+      start = start.next;
+    }
+    return true;
   };
 
   return list;
