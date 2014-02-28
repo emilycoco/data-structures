4c72a0e5982244b9fe0e2459c33b3c6c98814aaa
Hack Reactor Students <emilymcoco@gmail.com>
2014-02-28 15:40:36 -0800

    finished basic hash.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,16 +1,36 @@
 var HashTable = function(){
   this._limit = 8;
+  this._items = 0;
   this._storage = makeLimitedArray(this._limit);
 };
 
+HashTable.prototype.resize = function() {
+  if (this._items > (0.75 * this._limit)) {
+    this._limit *= 2;
+  }
+  if (this._items < 0.25 * this._limit) {
+    this._limit *= 0.5;
+  }
+}
+
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage[i] = v;
+  this._items ++;
+  // this.resize();
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage[i];
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage[i] = null;
+  if (this._items !== 0) {
+    this._items --;
+  }
+  // this.resize();
 };
 
