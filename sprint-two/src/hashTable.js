e7c56bf99cd5cc3d7f3f064d5c1a9298ceb5496e
Hack Reactor Students <emilymcoco@gmail.com>
2014-02-28 19:14:25 -0800

    now using provided api.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,27 +2,37 @@ var HashTable = function(){
   this._limit = 8;
   this._items = 0;
   this._storage = makeLimitedArray(this._limit);
+  console.log("storage: ", this._storage);
+  console.log(this._storage.each(function(value, key){
+    this._storage.get(key);
+  })
+  );
 };
 
 HashTable.prototype.resize = function() {
   if (this._items > (0.75 * this._limit)) {
     this._limit *= 2;
+    //this.rehash(this._storage);
   }
   if (this._items < 0.25 * this._limit) {
     this._limit *= 0.5;
+    //this.rehash(this._storage);
   }
-}
+};
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage[i] = v;
+  this._storage.set(i, v);
   this._items ++;
-  // this.resize();
+  this.resize();
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage[i];
+  if (this._storage.get(i) !== undefined) {
+    console.log(this._storage.get(i));
+    return this._storage.get(i);
+  }
 };
 
 HashTable.prototype.remove = function(k){
@@ -31,6 +41,15 @@ HashTable.prototype.remove = function(k){
   if (this._items !== 0) {
     this._items --;
   }
-  // this.resize();
+  this.resize();
 };
 
+/*HashTable.prototype.rehash = function(storageSet){
+  var tempStorage = storageSet['3'].slice(0);
+  //this._items = 0;
+  this._storage['3'] = [];
+  for(var i = 0; i < tempStorage.length; i++){
+    this.insert(tempStorage[i][0], tempStorage[i][1]);
+  }
+  console.log(this._items, this._limit);
+};*/
