3039e13101fdde9bd797c769c0839f94fa893524
Hack Reactor Students <emilymcoco@gmail.com>
2014-03-01 11:17:56 -0800

    creates buckets upon instantiation

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,13 +1,11 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = makeLimitedArray(this._limit);
-};
-
-HashTable.prototype.printer = function(){
+  for(var i = 0; i < this._limit; i++){
+    this._storage.set(i,[]);
+  }
   this._storage.printer();
 };
-
-
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
