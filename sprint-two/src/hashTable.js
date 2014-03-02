ace009a881951778482ec1b9c2e03a1ac169e6a3
Hack Reactor Students <emilymcoco@gmail.com>
2014-03-01 16:39:13 -0800

    Fixed part of hash.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,20 +1,48 @@
 var HashTable = function(){
   this._limit = 8;
+  this._count = 0;
   this._storage = makeLimitedArray(this._limit);
-  for(var i = 0; i < this._limit; i++){
-    this._storage.set(i,[]);
-  }
-  this._storage.printer();
 };
 
+
+// this is the one that caused the infinite loop:
+// change if bucket[h] to bucket [h][0] to stop infinite loop
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i) || [];
+  for (var h = 0; h < bucket.length; h++) {
+    var pair = bucket[h];
+    if (pair[0] === k) {
+      pair[1] = v;
+      return;
+    }
+  }
+  bucket.push([k,v]);
+  this._storage.set(i, bucket);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i) || [];
+  for (var h = 0; h < bucket.length; h++) {
+    var pair = bucket[h];
+    if (pair[0] === k) {
+      return pair[1];
+    }
+  }
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i) || [];
+  for (var h = 0; h < bucket.length; h++) {
+    var pair = bucket[h];
+    if (pair[0] === k) {
+      bucket.splice(pair, 1);
+    }
+    return pair[1];
+  }
+  return null;
 };
 
